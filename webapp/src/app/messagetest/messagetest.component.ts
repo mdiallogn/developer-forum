7a03c12152449ee4ae1c40e8ab923b2a2fa84a90
Mamadou Diallo <dialloakh@gmail.com>
2021-12-23 11:10:53 +0100

    Add notif in Frontend side

--- webapp/src/app/messagetest/messagetest.component.ts
+++ webapp/src/app/messagetest/messagetest.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Stomp} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import {Post} from "../model/post";
+import {FormBuilder} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-messagetest',
@@ -9,15 +13,20 @@ import * as SockJS from 'sockjs-client';
 })
 export class MessagetestComponent implements OnInit {
 
-  title = 'Web Socket Test';
-  description = 'Middleware Project';
+  title = 'Notifications using Web Sockets';
+  //description = 'Notifications';
+
+  notifications:Notification[] = []
+  baseUrl: string = "http://127.0.0.1:8000/api/v1/notifications";
 
   greetings: string[] = [];
   disabled = true;
   name: string | undefined;
   private stompClient = null;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private http: HttpClient,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -26,6 +35,7 @@ export class MessagetestComponent implements OnInit {
 
     if (connected) {
       this.greetings = [];
+     // this.notifications = []
     }
   }
 
@@ -46,6 +56,12 @@ export class MessagetestComponent implements OnInit {
         _this.showGreeting(JSON.parse(response.body));
       });
     });
+    this.http.get<Notification[]>(this.baseUrl).subscribe(
+      data => {
+        this.notifications = data
+      },
+      error => console.error("Notifications get error !")
+    );
   }
 
   disconnect() {
@@ -61,7 +77,7 @@ export class MessagetestComponent implements OnInit {
   sendName() {
     // @ts-ignore
     this.stompClient.send(
-      '/gkz/test',
+      '/api/v1/notifications',
       {},
       JSON.stringify({ 'name': this.name })
     );
