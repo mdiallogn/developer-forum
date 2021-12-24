75be3a907166884d5e7feddb7e044d074bc2ea41
saliou673 <diallom673@gmail.com>
2021-12-24 20:12:50 +0100

    perf: use mongodb atlas instead of local mongodb

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -3,17 +3,15 @@ package com.example.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableAutoConfiguration(exclude = {EmbeddedMongoAutoConfiguration.class})
 @EnableMongoRepositories
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={EmbeddedMongoAutoConfiguration.class})
 public class ServerApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
